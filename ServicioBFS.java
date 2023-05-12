dcafc0a4213b5c14d807f0512dd9597ef7bb0fb0
NahuCaroseli <nahucaroseli2100@gmail.com>
2023-05-11 22:44:43 -0300

    Añadido metodo bfsForest

--- ServicioBFS.java
+++ ServicioBFS.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class ServicioBFS {
 	
@@ -10,8 +13,39 @@ private Grafo<?> grafo;
 	}
 	
 	public List<Integer> bfsForest() {
-		// Resolver BFS
-		return new ArrayList<>();
+		List<Integer> vertices = new ArrayList<>();
+		Set<Integer> visitados = new HashSet<>();
+		List<Integer> fila = new ArrayList<>();
+		Iterator<Integer> it = this.grafo.obtenerVertices();
+		while(it.hasNext()) {
+			int vertice = it.next();
+			if(!visitados.contains(vertice)) {
+				bfs(vertice,vertices,fila,visitados);
+			}
+		}
+		return vertices;
 	}
+	
+	
+	private void bfs(int vertice, List<Integer> vertices,List<Integer> fila, Set<Integer> visitados) {
+		vertices.add(vertice);
+		visitados.add(vertice);
+		fila.add(vertice);
+		while(!fila.isEmpty()) {
+			int siguiente = fila.get(0);
+			fila.remove(0);
+			Iterator<Integer> it = grafo.obtenerAdyacentes(siguiente);
+			while(it.hasNext()) {
+				int adyacente = it.next();
+				if(!visitados.contains(adyacente)) {
+					visitados.add(adyacente);
+					fila.add(adyacente);
+				}
+			}
+		}
+	}
+	
+	
+	
 
 }
